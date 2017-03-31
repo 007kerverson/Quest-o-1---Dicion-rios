2c64d733e37ef5c6efd3cb0f268010879d958dc7
Kerverson  Gomes <kerversongomes@gmail.com>
2017-03-31 15:26:22 -0300

    Create Questão 1.py

---
+++ Questão 1.py
@@ -0,0 +1,4 @@
+dias_semana = { 1 : 'Domingo' , 2 : "Segunda", 3 : "Terça", 4 : "Quarta", 5 : "Quinta", 6 : "Sexta" , 7 : "Sábado"}
+print(dias_semana)
+dia = (dias_semana[int(input("Informe o número do Dia que deseja de 1 até 7:"))])
+print(dia)
